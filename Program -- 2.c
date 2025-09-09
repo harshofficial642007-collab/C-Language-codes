fd084bf2d0b81b147edf8ac427c96367c289aed4
HARSHVEER SINGH <harsh.official642007@gmail.com>
2025-09-09 14:00:20 +0530

    Add files via upload

---
+++ Program -- 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+	
+	int x =2;
+	switch (x)
+	{
+		case 1:printf(" choice is 1 :");
+			break;
+		case 2: printf(" choice is 2 :");
+		    break;
+		default: printf("choice not ");
+			break;
+	}
+	return 0;
+}
